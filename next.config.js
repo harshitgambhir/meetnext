654390b35db0ee69f666bc229b0bc5da863fab58
harshitgambhir <harshitgambhir88@gmail.com>
2021-12-15 23:19:12 +0530

    final

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   reactStrictMode: true,
   images: {
-    domains: ['inmeet-dev.s3.ap-south-1.amazonaws.com', 'lh3.googleusercontent.com', '192.168.0.155'],
+    domains: ['inmeet-prod.s3.ap-south-1.amazonaws.com', 'inmeet-dev.s3.ap-south-1.amazonaws.com', 'lh3.googleusercontent.com', '192.168.0.155'],
   },
 }
